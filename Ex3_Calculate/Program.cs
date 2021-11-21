530ecb70ce91b4e59372bb81cdf4831cff4a03d2
eugdov <eugene.dov@gmail.com>
2021-11-21 16:27:34 +0300

    Random added to Example 3

--- Ex3_Calculate/Program.cs
+++ Ex3_Calculate/Program.cs
@@ -1,4 +1,6 @@
-﻿int NumberA = 8;
-int NumberB = 6;
+﻿int NumberA = new Random().Next(1,10);
+Console.WriteLine(NumberA);
+int NumberB = new Random().Next(1,10);
+Console.WriteLine(NumberB);
 int result = NumberA + NumberB;
 Console.WriteLine(result);
